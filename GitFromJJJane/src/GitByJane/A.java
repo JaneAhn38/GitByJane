17bf3619800ce07a5a3ea0bb61e4415dc5da9d7d
JaneAhn38 <jjung9874@gmail.com>
2025-12-14 19:10:47 +0900

    New Bob came out

--- GitFromJJJane/src/GitByJane/A.java
+++ GitFromJJJane/src/GitByJane/A.java
@@ -3,6 +3,7 @@ package GitByJane;
 public class A {
 	public A() {
 		System.out.println("Hello Hello world");
+		System.out.println("Hi im Bob");
 	}
 
 
--- GitFromJJJane/src/GitByJane/B.java
+++ GitFromJJJane/src/GitByJane/B.java
@@ -3,6 +3,7 @@ package GitByJane;
 public class B {
 	public B () {
 		System.out.println("Hi this is B");
+		System.out.println("Hi this is Bob also");
 	}
 
 }
